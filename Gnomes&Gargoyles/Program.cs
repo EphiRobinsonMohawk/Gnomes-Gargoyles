3531d358f9fd9cf04ad0786efe9efa68f4b35fbe
Ephi Robinson <000969115@mohawkcollege.ca>
2025-10-07 13:51:11 -0400

    Arrays

--- Gnomes&Gargoyles/Program.cs
+++ Gnomes&Gargoyles/Program.cs
@@ -5,6 +5,45 @@ class GridShell
     const int Rows = 9;
     const int Columns = 5;
 
+    public char[] Row1;
+    public char[] Row2;
+    public char[] Row3;
+    public char[] Row4;
+    public char[] Row5;
+    public char[] Row6;
+    public char[] Row7;
+    public char[] Row8;
+    public char[] Row9;
+    public char[] Row10;
+    public char[] Row11;
+    public char[] Row12;
+    public char[] Row13;
+    public char[] Row14;
+    public char[] Row15;
+    public char[] Row16;
+    public char[] Row17;
+    public char[] Row18;
+    public char[] Row19;
+    public char[] Row20;
+    public char[] Row21;
+    public char[] Row22;
+    public char[] Row23;
+    public char[] Row24;
+    public char[] Row25;
+    public char[] Row26;
+    public char[] Row27;
+    public char[] Row28;
+
+    void Start()
+    {
+
+
+    string[] Lines = [
+        (Row1(1) )
+        ]; 
+    }
+
+
     static void Main()
     {
         DrawGridShell();
