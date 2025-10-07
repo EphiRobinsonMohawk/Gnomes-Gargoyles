df953e07d52dd51c463ad854932f111407238057
Ephi Robinson <000969115@mohawkcollege.ca>
2025-10-07 14:57:34 -0400

    Update

--- Gnomes&Gargoyles/Program.cs
+++ Gnomes&Gargoyles/Program.cs
@@ -1,81 +1,92 @@
 ﻿using System;
+using System.Data;
 
 class GridShell
 {
-    const int Rows = 9;
-    const int Columns = 5;
+    //const int Rows = 9;
+    //const int Columns = 5;
+    //Character arrays for each line
+    static char[] Row1 = { '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+' };
+    static char[] Row2 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row3 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row4 = { '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+' };
+    static char[] Row5 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row6 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row7 = { '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+' };
+    static char[] Row8 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row9 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row10 = { '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+' };
+    static char[] Row11 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row12 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row13 = { '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+' };
+    static char[] Row14 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row15 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row16 = { '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+' };
+    static char[] Row17 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row18 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row19 = { '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+' };
+    static char[] Row20 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row21 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row22 = { '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+' };
+    static char[] Row23 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row24 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row25 = { '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+' };
+    static char[] Row26 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row27 = { '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|' };
+    static char[] Row28 = { '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '+' };
 
-    public char[] Row1;
-    public char[] Row2;
-    public char[] Row3;
-    public char[] Row4;
-    public char[] Row5;
-    public char[] Row6;
-    public char[] Row7;
-    public char[] Row8;
-    public char[] Row9;
-    public char[] Row10;
-    public char[] Row11;
-    public char[] Row12;
-    public char[] Row13;
-    public char[] Row14;
-    public char[] Row15;
-    public char[] Row16;
-    public char[] Row17;
-    public char[] Row18;
-    public char[] Row19;
-    public char[] Row20;
-    public char[] Row21;
-    public char[] Row22;
-    public char[] Row23;
-    public char[] Row24;
-    public char[] Row25;
-    public char[] Row26;
-    public char[] Row27;
-    public char[] Row28;
+    static void Main()
+    {
+        while (true)
+        {
+            Update();
 
-    void Update() //Happens Every Frame
+            // Press ESC to break out
+            if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape)
+                break;
+
+            Thread.Sleep(500); // 2 FPS
+        }
+    }
+    static void Update() //Happens Every Frame
     { //Clearing the past frame's lines before printing new lines 
         Console.Clear();
-        { //Printing each line
-            Console.WriteLine(Row1[1] + Row1[2] + Row1[3] + Row1[4] + Row1[5] + Row1[6] + Row1[7] + Row1[8] + Row1[9] + Row1[10] + Row1[11] + Row1[12] + Row1[13] + Row1[14] + Row1[15] + Row1[16] + Row1[17] + Row1[18] + Row1[19] + Row1[20] + Row1[21]);
-            Console.WriteLine(Row2[1] + Row2[2] + Row2[3] + Row2[4] + Row2[5] + Row2[6] + Row2[7] + Row2[8] + Row2[9] + Row2[10] + Row2[11] + Row2[12] + Row2[13] + Row2[14] + Row2[15] + Row2[16] + Row2[17] + Row2[18] + Row2[19] + Row2[20] + Row2[21]);
-            Console.WriteLine(Row3[1] + Row3[2] + Row3[3] + Row3[4] + Row3[5] + Row3[6] + Row3[7] + Row3[8] + Row3[9] + Row3[10] + Row3[11] + Row3[12] + Row3[13] + Row3[14] + Row3[15] + Row3[16] + Row3[17] + Row3[18] + Row3[19] + Row3[20] + Row3[21]);
-            Console.WriteLine(Row4[1] + Row4[2] + Row4[3] + Row4[4] + Row4[5] + Row4[6] + Row4[7] + Row4[8] + Row4[9] + Row4[10] + Row4[11] + Row4[12] + Row4[13] + Row4[14] + Row4[15] + Row4[16] + Row4[17] + Row4[18] + Row4[19] + Row4[20] + Row4[21]);
-            Console.WriteLine(Row5[1] + Row5[2] + Row5[3] + Row5[4] + Row5[5] + Row5[6] + Row5[7] + Row5[8] + Row5[9] + Row5[10] + Row5[11] + Row5[12] + Row5[13] + Row5[14] + Row5[15] + Row5[16] + Row5[17] + Row5[18] + Row5[19] + Row5[20] + Row5[21]);
-            Console.WriteLine(Row6[1] + Row6[2] + Row6[3] + Row6[4] + Row6[5] + Row6[6] + Row6[7] + Row6[8] + Row6[9] + Row6[10] + Row6[11] + Row6[12] + Row6[13] + Row6[14] + Row6[15] + Row6[16] + Row6[17] + Row6[18] + Row6[19] + Row6[20] + Row6[21]);
-            Console.WriteLine(Row7[1] + Row7[2] + Row7[3] + Row7[4] + Row7[5] + Row7[6] + Row7[7] + Row7[8] + Row7[9] + Row7[10] + Row7[11] + Row7[12] + Row7[13] + Row7[14] + Row7[15] + Row7[16] + Row7[17] + Row7[18] + Row7[19] + Row7[20] + Row7[21]);
-            Console.WriteLine(Row8[1] + Row8[2] + Row8[3] + Row8[4] + Row8[5] + Row8[6] + Row8[7] + Row8[8] + Row8[9] + Row8[10] + Row8[11] + Row8[12] + Row8[13] + Row8[14] + Row8[15] + Row8[16] + Row8[17] + Row8[18] + Row8[19] + Row8[20] + Row8[21]);
-            Console.WriteLine(Row9[1] + Row9[2] + Row9[3] + Row9[4] + Row9[5] + Row9[6] + Row9[7] + Row9[8] + Row9[9] + Row9[10] + Row9[11] + Row9[12] + Row9[13] + Row9[14] + Row9[15] + Row9[16] + Row9[17] + Row9[18] + Row9[19] + Row9[20] + Row9[21]);
-            Console.WriteLine(Row10[1] + Row10[2] + Row10[3] + Row10[4] + Row10[5] + Row10[6] + Row10[7] + Row10[8] + Row10[9] + Row10[10] + Row10[11] + Row10[12] + Row10[13] + Row10[14] + Row10[15] + Row10[16] + Row10[17] + Row10[18] + Row10[19] + Row10[20] + Row10[21]);
-            Console.WriteLine(Row11[1] + Row11[2] + Row11[3] + Row11[4] + Row11[5] + Row11[6] + Row11[7] + Row11[8] + Row11[9] + Row11[10] + Row11[11] + Row11[12] + Row11[13] + Row11[14] + Row11[15] + Row11[16] + Row11[17] + Row11[18] + Row11[19] + Row11[20] + Row11[21]);
-            Console.WriteLine(Row12[1] + Row12[2] + Row12[3] + Row12[4] + Row12[5] + Row12[6] + Row12[7] + Row12[8] + Row12[9] + Row12[10] + Row12[11] + Row12[12] + Row12[13] + Row12[14] + Row12[15] + Row12[16] + Row12[17] + Row12[18] + Row12[19] + Row12[20] + Row12[21]);
-            Console.WriteLine(Row13[1] + Row13[2] + Row13[3] + Row13[4] + Row13[5] + Row13[6] + Row13[7] + Row13[8] + Row13[9] + Row13[10] + Row13[11] + Row13[12] + Row13[13] + Row13[14] + Row13[15] + Row13[16] + Row13[17] + Row13[18] + Row13[19] + Row13[20] + Row13[21]);
-            Console.WriteLine(Row14[1] + Row14[2] + Row14[3] + Row14[4] + Row14[5] + Row14[6] + Row14[7] + Row14[8] + Row14[9] + Row14[10] + Row14[11] + Row14[12] + Row14[13] + Row14[14] + Row14[15] + Row14[16] + Row14[17] + Row14[18] + Row14[19] + Row14[20] + Row14[21]);
-            Console.WriteLine(Row15[1] + Row15[2] + Row15[3] + Row15[4] + Row15[5] + Row15[6] + Row15[7] + Row15[8] + Row15[9] + Row15[10] + Row15[11] + Row15[12] + Row15[13] + Row15[14] + Row15[15] + Row15[16] + Row15[17] + Row15[18] + Row15[19] + Row15[20] + Row15[21]);
-            Console.WriteLine(Row16[1] + Row16[2] + Row16[3] + Row16[4] + Row16[5] + Row16[6] + Row16[7] + Row16[8] + Row16[9] + Row16[10] + Row16[11] + Row16[12] + Row16[13] + Row16[14] + Row16[15] + Row16[16] + Row16[17] + Row16[18] + Row16[19] + Row16[20] + Row16[21]);
-            Console.WriteLine(Row17[1] + Row17[2] + Row17[3] + Row17[4] + Row17[5] + Row17[6] + Row17[7] + Row17[8] + Row17[9] + Row17[10] + Row17[11] + Row17[12] + Row17[13] + Row17[14] + Row17[15] + Row17[16] + Row17[17] + Row17[18] + Row17[19] + Row17[20] + Row17[21]);
-            Console.WriteLine(Row18[1] + Row18[2] + Row18[3] + Row18[4] + Row18[5] + Row18[6] + Row18[7] + Row18[8] + Row18[9] + Row18[10] + Row18[11] + Row18[12] + Row18[13] + Row18[14] + Row18[15] + Row18[16] + Row18[17] + Row18[18] + Row18[19] + Row18[20] + Row18[21]);
-            Console.WriteLine(Row19[1] + Row19[2] + Row19[3] + Row19[4] + Row19[5] + Row19[6] + Row19[7] + Row19[8] + Row19[9] + Row19[10] + Row19[11] + Row19[12] + Row19[13] + Row19[14] + Row19[15] + Row19[16] + Row19[17] + Row19[18] + Row19[19] + Row19[20] + Row19[21]);
-            Console.WriteLine(Row20[1] + Row20[2] + Row20[3] + Row20[4] + Row20[5] + Row20[6] + Row20[7] + Row20[8] + Row20[9] + Row20[10] + Row20[11] + Row20[12] + Row20[13] + Row20[14] + Row20[15] + Row20[16] + Row20[17] + Row20[18] + Row20[19] + Row20[20] + Row20[21]);
-            Console.WriteLine(Row21[1] + Row21[2] + Row21[3] + Row21[4] + Row21[5] + Row21[6] + Row21[7] + Row21[8] + Row21[9] + Row21[10] + Row21[11] + Row21[12] + Row21[13] + Row21[14] + Row21[15] + Row21[16] + Row21[17] + Row21[18] + Row21[19] + Row21[20] + Row21[21]);
-            Console.WriteLine(Row22[1] + Row22[2] + Row22[3] + Row22[4] + Row22[5] + Row22[6] + Row22[7] + Row22[8] + Row22[9] + Row22[10] + Row22[11] + Row22[12] + Row22[13] + Row22[14] + Row22[15] + Row22[16] + Row22[17] + Row22[18] + Row22[19] + Row22[20] + Row22[21]);
-            Console.WriteLine(Row23[1] + Row23[2] + Row23[3] + Row23[4] + Row23[5] + Row23[6] + Row23[7] + Row23[8] + Row23[9] + Row23[10] + Row23[11] + Row23[12] + Row23[13] + Row23[14] + Row23[15] + Row23[16] + Row23[17] + Row23[18] + Row23[19] + Row23[20] + Row23[21]);
-            Console.WriteLine(Row24[1] + Row24[2] + Row24[3] + Row24[4] + Row24[5] + Row24[6] + Row24[7] + Row24[8] + Row24[9] + Row24[10] + Row24[11] + Row24[12] + Row24[13] + Row24[14] + Row24[15] + Row24[16] + Row24[17] + Row24[18] + Row24[19] + Row24[20] + Row24[21]);
-            Console.WriteLine(Row25[1] + Row25[2] + Row25[3] + Row25[4] + Row25[5] + Row25[6] + Row25[7] + Row25[8] + Row25[9] + Row25[10] + Row25[11] + Row25[12] + Row25[13] + Row25[14] + Row25[15] + Row25[16] + Row25[17] + Row25[18] + Row25[19] + Row25[20] + Row25[21]);
-            Console.WriteLine(Row26[1] + Row26[2] + Row26[3] + Row26[4] + Row26[5] + Row26[6] + Row26[7] + Row26[8] + Row26[9] + Row26[10] + Row26[11] + Row26[12] + Row26[13] + Row26[14] + Row26[15] + Row26[16] + Row26[17] + Row26[18] + Row26[19] + Row26[20] + Row26[21]);
-            Console.WriteLine(Row27[1] + Row27[2] + Row27[3] + Row27[4] + Row27[5] + Row27[6] + Row27[7] + Row27[8] + Row27[9] + Row27[10] + Row27[11] + Row27[12] + Row27[13] + Row27[14] + Row27[15] + Row27[16] + Row27[17] + Row27[18] + Row27[19] + Row27[20] + Row27[21]);
-            Console.WriteLine(Row28[1] + Row28[2] + Row28[3] + Row28[4] + Row28[5] + Row28[6] + Row28[7] + Row28[8] + Row28[9] + Row28[10] + Row28[11] + Row28[12] + Row28[13] + Row28[14] + Row28[15] + Row28[16] + Row28[17] + Row28[18] + Row28[19] + Row28[20] + Row28[21]);
+        { //Printing each line (Brackets solely so you can collapse it) 
+            Console.WriteLine(new string(Row1));
+            Console.WriteLine(new string(Row2));
+            Console.WriteLine(new string(Row3));
+            Console.WriteLine(new string(Row4));
+            Console.WriteLine(new string(Row5));
+            Console.WriteLine(new string(Row6));
+            Console.WriteLine(new string(Row7));
+            Console.WriteLine(new string(Row8));
+            Console.WriteLine(new string(Row9));
+            Console.WriteLine(new string(Row10));
+            Console.WriteLine(new string(Row11));
+            Console.WriteLine(new string(Row12));
+            Console.WriteLine(new string(Row13));
+            Console.WriteLine(new string(Row14));
+            Console.WriteLine(new string(Row15));
+            Console.WriteLine(new string(Row16));
+            Console.WriteLine(new string(Row17));
+            Console.WriteLine(new string(Row18));
+            Console.WriteLine(new string(Row19));
+            Console.WriteLine(new string(Row20));
+            Console.WriteLine(new string(Row21));
+            Console.WriteLine(new string(Row22));
+            Console.WriteLine(new string(Row23));
+            Console.WriteLine(new string(Row24));
+            Console.WriteLine(new string(Row25));
+            Console.WriteLine(new string(Row26));
+            Console.WriteLine(new string(Row27));
+            Console.WriteLine(new string(Row28));
         }
         
     }
 
-
-    static void Main()
-    {
-        DrawGridShell();
-    }
-
+    
+    
+    /*
     static void DrawGridShell()
     {
         Console.Clear();
@@ -109,5 +120,5 @@ class GridShell
             Console.Write("|   ");
         }
         Console.WriteLine("|");
-    }
+    } */
 }
