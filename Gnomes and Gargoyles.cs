cff02b45d2499ae7a78e8d5ddbb71486869d0e44
SebastianP2423 <000946282@mohawkcollege.ca>
2025-10-07 10:55:26 -0400

    Create Gnomes and Gargoyles.cs

---
+++ Gnomes and Gargoyles.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class GridShell
+{
+    const int Rows = 9;
+    const int Columns = 5;
+
+    static void Main()
+    {
+        DrawGridShell();
+    }
+
+    static void DrawGridShell()
+    {
+        Console.Clear();
+
+        for (int row = 0; row < Rows; row++)
+        {
+            // Top border of every single row
+            DrawHorizontalLine();
+
+            // Empty line so that te cells can be filled with ASCII text
+            DrawEmptyLine();
+        }
+
+        // Draw bottom border of the grid
+        DrawHorizontalLine();
+    }
+
+    static void DrawHorizontalLine()
+    {
+        for (int col = 0; col < Columns; col++) // Loop that prints the lines to create the horizontal part in the grid
+        {
+            Console.Write("+---"); 
+        }
+        Console.WriteLine("+"); // Outside of this loop this just finishes off the border 
+    }
+
+    static void DrawEmptyLine()  // Another loop that 
+    {
+        for (int col = 0; col < Columns; col++)
+        {
+            Console.Write("|   ");
+        }
+        Console.WriteLine("|");
+    }
+}
